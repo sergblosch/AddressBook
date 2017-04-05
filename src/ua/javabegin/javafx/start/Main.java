919d78aa92a507037c34abc04b7a9b38a3dc7785
sergblosch <sbloshchenko@mail.ru>
2017-04-05 16:35:00 +0300

    Main.java was modified

--- src/ua/javabegin/javafx/start/Main.java
+++ src/ua/javabegin/javafx/start/Main.java
@@ -2,7 +2,6 @@ package ua.javabegin.javafx.start;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
